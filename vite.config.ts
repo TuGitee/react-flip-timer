3be04da150b11cf97e82b124dcfb454ac52c9881
TuGitee <tgb@std.uestc.edu.cn>
2025-04-05 22:15:58 +0800

    build: web page

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 
+const isWeb = process.env.BUILD_MODE === 'web';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ 
+  plugins: [react(), dts({
     include: ['src'],
-    rollupTypes: true 
+    rollupTypes: true
   })],
   build: {
-    lib: {
+    lib: isWeb ? undefined : {
       entry: 'src/index.ts',
       formats: ['es', 'cjs'],
       fileName: (format) => `index.${format}.js`
